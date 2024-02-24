591ebf4a0751047740ce5b87c835eba2e67c08c9
Paramvir Singh <>
2024-02-24 16:32:17 -0500

    - added shadcn button for styling

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+export const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold text-orange-500 hover:text-white hover:bg-orange-500"
+    >
+      Log In
+    </Button>
+  );
+};
---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,41 @@
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Menu } from "lucide-react";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span>Welcome to MernEats.com</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500 mb-10">
+            Log In
+          </Button>
+        </SheetDescription>
+
+        <SheetFooter>
+          <SheetClose className="flex">
+            <Button className="flex-1 font-bold bg-blue-400 py-4">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
